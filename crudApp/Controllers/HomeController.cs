ae15842cce656008c161185a18b3766d3da2d8d8
ppaul450 <ppaul450@gmail.com>
2023-12-29 01:29:45 +0530

    added home controller and changed the program.cs file to support MVC

---
+++ crudApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace crudApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- crudApp/Program.cs
+++ crudApp/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+
+app.UseRouting();
+app.MapControllerRoute(
+    name:"default",
+    pattern: "{Controller:Home}/{action:Index}/{id?}");
+
+
+
+
 
 app.Run();
